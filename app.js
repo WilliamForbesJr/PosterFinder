576d45de61879c04bdeb2c42a7a433f2a21b6fdc
Will Forbes <42383119+WilliamForbesJr@users.noreply.github.com>
2019-09-19 23:32:32 -0400

    Add files via upload
    Refactor codebase for modern JS syntax. Clean up code for readability. Update UI

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+const express = require("express");
+const app = express();
+const request = require("request");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("search");
 });
 
-app.get("/results", function(req, res){
-    var query = req.query.search;
-    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
-   request(url, function(error,response,body){
+app.get("/results", (req, res) => {
+
+    const query = req.query.search;
+    const url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+
+   request(url, (error,response,body) => {
+
        if(!error && response.statusCode == 200){
-           var data = JSON.parse(body);
+           const data = JSON.parse(body);
            res.render("results", {data: data});
         }
    });
 });
 
-
-
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, () => {
     console.log("Movie App has started!");
 });
